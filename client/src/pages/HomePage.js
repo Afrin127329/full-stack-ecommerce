5717c52f1e7158c8b9d7cda7201c82550bc2e0ad
Afrin Nahar <afrinnahar@gmail.com>
2023-06-21 21:38:10 +0600

    latest commit

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -157,24 +157,24 @@ const HomePage = () => {
           <h1 className="text-center">All Products</h1>
           <div className="d-flex card-flex  flex-wrap">
             {products?.map((p) => (
-              <div className="card m-2" key={p._id}>
+              <div className="card m-2" key={p?._id}>
                 <img
-                  src={`/api/v1/product/product-photo/${p._id}`}
+                  src={`/api/v1/product/product-photo/${p?._id}`}
                   className="card-img-top"
-                  alt={p.name}
+                  alt={p?.name}
                 />
                 <div className="card-body">
                   <div className="card-name-price">
                     <h5 className="card-title">{p.name}</h5>
                     <h5 className="card-title card-price">
-                      {p.price.toLocaleString("en-US", {
+                      {p?.price?.toLocaleString("en-US", {
                         style: "currency",
                         currency: "USD",
                       })}
                     </h5>
                   </div>
                   <p className="card-text ">
-                    {p.description.substring(0, 60)}...
+                    {p?.description?.substring(0, 60)}...
                   </p>
                   <div className="card-name-price">
                     <button
